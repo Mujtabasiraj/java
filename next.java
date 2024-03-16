d3dc58a1dee861f07f86e326439660980a3f962d
Mujtabasiraj <149222185+Mujtabasiraj@users.noreply.github.com>
2024-03-16 15:06:17 +0530

    Add files via upload

---
+++ next.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class next
+       {
+               public static void main(String[] p)
+               {
+                       Scanner sc=new Scanner(System.in);
+                       System.out.println("enter your name");
+                       String name= sc.next();
+                       System.out.println("welcome\t"+name);
+               }
+        }
