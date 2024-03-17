e8ea8c84aac2999e305a3743330addcef57d9546
MdInam7200 <149220325+Md-Inam@users.noreply.github.com>
2024-03-17 12:45:03 +0530

    Update g.java

--- g.java
+++ g.java
@@ -3,5 +3,6 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
       System.out.println("JAVA WAS DEVELOPED BY JAMES GOSLING");
+        System.out.println("JAVA WAS DEVELOPED BY JAMES GOSLING");
   }
 }
